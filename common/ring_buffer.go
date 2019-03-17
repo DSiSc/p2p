e05ba8b0488d1d44ee9b9af1832a5e8e60dcabdc
bchain618 <bchain618@gmail.com>
2019-03-17 16:27:53 +0800

    bugfix# fix ring buffer overflow bug
    1.remove unused node from record list to limit the ring buffer's list
    size

--- common/ring_buffer.go
+++ common/ring_buffer.go
@@ -6,9 +6,15 @@ import (
 	"sync"
 )
 
+// ringRecord represent a record in ring buffer
+type ringRecord struct {
+	v    interface{}
+	node *list.Element
+}
+
 // RingBuffer is a ring buffer implementation.
 type RingBuffer struct {
-	elements map[types.Hash]interface{}
+	elements map[types.Hash]*ringRecord
 	limit    int
 	keyList  *list.List
 	lock     sync.RWMutex
@@ -17,7 +23,7 @@ type RingBuffer struct {
 // NewRingBuffer create a ring buffer instance
 func NewRingBuffer(size int) *RingBuffer {
 	return &RingBuffer{
-		elements: make(map[types.Hash]interface{}),
+		elements: make(map[types.Hash]*ringRecord),
 		limit:    size,
 		keyList:  list.New(),
 	}
@@ -27,11 +33,18 @@ func NewRingBuffer(size int) *RingBuffer {
 func (ring *RingBuffer) AddElement(hash types.Hash, elem interface{}) {
 	ring.lock.Lock()
 	defer ring.lock.Unlock()
-	ring.keyList.PushFront(hash)
-	ring.elements[hash] = elem
+	if r, ok := ring.elements[hash]; ok {
+		ring.keyList.Remove(r.node)
+	}
+	first := ring.keyList.PushFront(hash)
+	ring.elements[hash] = &ringRecord{
+		v:    elem,
+		node: first,
+	}
 	if len(ring.elements) > ring.limit {
-		node := ring.keyList.Back()
-		delete(ring.elements, node.Value.(types.Hash))
+		last := ring.keyList.Back()
+		delete(ring.elements, last.Value.(types.Hash))
+		ring.keyList.Remove(last)
 	}
 }
 
@@ -39,5 +52,5 @@ func (ring *RingBuffer) AddElement(hash types.Hash, elem interface{}) {
 func (ring *RingBuffer) Exist(hash types.Hash) bool {
 	ring.lock.RLock()
 	defer ring.lock.RUnlock()
-	return ring.elements[hash] != nil
+	return nil != ring.elements[hash]
 }
